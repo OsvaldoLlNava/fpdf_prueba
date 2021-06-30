561e17fd77d14cccd45781a9e0659827c2149a99
LOsvaldoLlNava <luis_llanesn@hotmail.com>
2021-06-30 10:23:35 -0500

    repo añadido

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/OsvaldoLlNava/fpdf_prueba
+
+go 1.16
+
+require github.com/jung-kurt/gofpdf v1.16.2
---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+// programa obtenido de https://appliedgo.net/pdf/
+// este diseño esta pensado solo para un pdf simple de un archivo especifico
+// tiene que ser modificado depende de la cantidad de columnas
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
+func main() {
+	// aqui primero se carga el archivo CSV
+	// la funcion path() solo es un pequeño intermedio para obtener el path de os.Args
+	data := loadCSV(path())
+
+	// una vez tenemos la informacion se crea un documento pdf
+	pdf := newReport()
+	pdf = image(pdf)
+	pdf = header(pdf, data[0])
+	pdf = table(pdf, data[1:])
+
+	if pdf.Err() {
+		log.Fatalf("Failed creating PDF report: %s\n", pdf.Error())
+	}
+
+	err := savePDF(pdf, "reporte")
+	if err != nil {
+		log.Fatalf("Cannot save PDF: %s|n", err)
+	}
+}
+
+func loadCSV(path string) [][]string {
+	// en este punto se obtiene toda la informacion del CSV,
+	// se abre el archivo, se lee, y se regresan las filas del archivo leido
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Cannot open '%s': %s\n", path, err.Error())
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+	rows, err := r.ReadAll()
+	if err != nil {
+		log.Fatalln("Cannot read CSV data:", err.Error())
+	}
+	return rows
+}
+
+func path() string {
+	if len(os.Args) < 2 {
+		return "cities.csv" // aqui es donde se da el nombre del archivo que se usara si no se entrega algun argumento
+	}
+	return os.Args[1]
+}
+
+func newReport() *gofpdf.Fpdf {
+	// la creacion del pdf es simple, y esta dado por:
+	// .New(
+	// 	"Orientacion",  esta puede ser "L" landscape (paisaje: horizontal) o  "P" portrait (retrato: vertical) "" = "P"
+	//	"Unidad de medida", esta es la unidad de medida en la que se pasaran los tamaños ("pt", "cm", "in", "mm") "" = "mm"
+	//	"Formato", esto se refiere a el formato de la hoja del pdf ("A3", "A4", "A5", "Letter", "Legal", "Tabloid") "" = "A4"
+	//	"Font", este valor es una direccion hacia un directorio para usar alguna font especifica descargada
+	// )
+	// Los valores vacios tienen asignados una opcion default cada uno
+	pdf := gofpdf.New("L", "mm", "Letter", "")
+
+	pdf.AddPage()
+
+	// set font te establece el tipo de letra que se usara, ya sea un tipo estandart o,
+	// algun tipo agregado con addFont o AddFontFromReader, etc.
+
+	// se construye:
+	// .SetFont(
+	//	"familyStr", Este parametro recibe el tipo de letra que se quiere usar
+	// 	"styleStr", puede ser "B"(negritas), "I"(cursiva/inclinada), "U"(subrayado), "S"(tachado)
+	// )
+	pdf.SetFont("Times", "B", 28)
+
+	//.Cell es la version simple de CellFormat,
+	// solo se tienenn los tamaños, el texto y listo, no tiene bordes, ni aliniacion ni nada de eso
+	pdf.Cell(40, 10, "Daily Report")
+
+	pdf.Ln(12) // .Ln es un salto de linea, se le ingresa la distancia que se movera
+
+	pdf.SetFont("Times", "", 20)
+	pdf.Cell(40, 10, time.Now().Format("Mon Jan 2, 2006"))
+	pdf.Ln(20)
+
+	return pdf
+
+}
+
+func header(pdf *gofpdf.Fpdf, hdr []string) *gofpdf.Fpdf {
+	pdf.SetFont("Times", "B", 16)
+	pdf.SetFillColor(240, 240, 240)
+	for i, str := range hdr {
+
+		if i != 8 {
+			pdf.CellFormat(20, 7, str, "1", 0, "C", false, 0, "")
+		} else {
+			pdf.CellFormat(45, 7, str, "1", 0, "C", false, 0, "")
+		}
+	}
+	pdf.Ln(-1)
+	return pdf
+}
+
+func table(pdf *gofpdf.Fpdf, tbl [][]string) *gofpdf.Fpdf {
+	pdf.SetFont("Times", "", 16)
+	pdf.SetFillColor(255, 255, 255)
+
+	// en este caso la variable align es para establecer multiples alinamientos en la tabla (dependiendo que casilla sea)
+	// esto es una forma muy estricta conforme a el numero de columnas,
+	// align debe de tener elementos igual o mayor al numero de columnas que tendra la tabla
+	// en caso de cities que todo es centrado, no se necesita de esta variable
+
+	// align := []string{"L", "C", "L", "R", "R", "R"} // align para queso.csv
+
+	for _, line := range tbl {
+
+		// for i, str := range line { // for para queso.csv
+		// 	pdf.CellFormat(25, 7, str, "1", 0, align[i], false, 0, "")
+		// }
+
+		for i, str := range line { // for para cities
+			if i != 8 {
+				pdf.CellFormat(20, 7, str, "1", 0, "C", false, 0, "")
+			} else {
+				pdf.CellFormat(45, 7, str, "1", 0, "C", false, 0, "")
+			}
+
+		}
+		pdf.Ln(-1)
+	}
+	return pdf
+}
+
+func image(pdf *gofpdf.Fpdf) *gofpdf.Fpdf {
+
+	// para colocar una imagen, puedes usar .ImageOptions,
+	// .ImageOptions usa siempre la pagina actual, los parametros que usa son
+	// "ImageName", recibe un string con el nombre de la imagen a usar
+	// "x", "y" son la posicion dentro de la pagina actual donde se colocara la imagen
+	// "h", "w" son los tamaños que tomara la imagen
+	// "flow", si es verdadero, el valor de "y" actual se desplaza y puede hacer un salto de pagina (verdadero  hace salto de linea, falso se conserva la linea actual)
+	// "options", es un struct para opciones adicionales que se le pueden agregar
+
+	pdf.ImageOptions("stats.png", 225, 10, 25, 25, false, gofpdf.ImageOptions{ImageType: "PNG", ReadDpi: true}, 0, "")
+	return pdf
+}
+
+func savePDF(pdf *gofpdf.Fpdf, name string) error {
+	fileName := fmt.Sprintf("%s.pdf", name)
+	return pdf.OutputFileAndClose(fileName)
+}
